d8f59d5842c7bc9ab3f84cf1b99a61c866346f1a
Josuezguevara <josuezelaya99@hotmail.es>
2017-04-28 17:09:34 -0600

    LAb2

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 int menu();
 int factorial(int); 
 float e1(int);
+double distancia(int,int,int,int);
+
 int main(){
  bool salir=false;
  while ( !salir ) {
@@ -19,8 +21,90 @@ int main(){
    break;
    }
    case 2:{
+    int a1, a2, b1, b2, c1, c2, d1, d2;
+    cout<< "Ingrese la coordenada X de A: "<<endl;
+    cin>>a1;
+    cout<< "Ingrese la coordenada Y de A: "<<endl;
+    cin>>a2;
+    cout<< "Ingrese la coordenada X de B: "<<endl;
+    cin>>b1;
+    cout<< "Ingrese la coordenada Y de B: "<<endl;
+    cin>>b2;
+    cout<< "Ingrese la coordenda X de C: "<<endl;
+    cin>>c1;
+    cout<< "Ingrese la coordenada Y de C: "<<endl;
+    cin>>c2;
+    cout<< "Ingrese la coordenada X de D: "<<endl;
+    cin>>d1;
+    cout<< "Ingrese la coordenada Y de D: "<<endl;
+    cin>>d2;
+    cout<< "Los puntos son: "<<endl;
+    cout<< "("<<a1<<","<<a2<<")"<<endl;
+    cout<< "("<<b1<<","<<b2<<")"<<endl;
+    cout<< "("<<c1<<","<<c2<<")"<<endl;
+    cout<< "("<<d1<<","<<d2<<")"<<endl;
 
-   break;
+    double cd, db, ba, ac, ad;
+    cd=distancia(c1,c2,d1,d2);
+    db=distancia(d1,d2,b1,b2);
+    ba=distancia(b1,b2,a1,a2);
+    ac=distancia(a1,a2,c1,c2);
+    ad=distancia(a1,a2,d1,d2);
+
+    cout<<"Los lados del trapezoide miden: "<<endl;
+    cout<<"Lado 1: "<<cd<<endl;
+    cout<<"Lado 2: "<<db<<endl;
+    cout<<"Lado 3: "<<ba<<endl;
+    cout<<"Lado 4: "<<ac<<endl;
+
+    cout<<"los lados del triangulo 1 miden: "<<endl;
+    cout<<"Lado 1: "<<cd<<endl;
+    cout<<"Lado 2: "<<ac<<endl;
+    cout<<"Lado 3: "<<ad<<endl;
+
+    cout<<"Los lados del triangulo 2 miden: "<<endl;
+    cout<<"Lado 1: "<<db<<endl;
+    cout<<"Lado 2: "<<ba<<endl;
+    cout<<"Lado 3: "<<ad<<endl;
+    
+    double semit1,semit2;//variable de semiperimetros de los trianngulos
+    double perit1,perit2;//variable de perimetros
+
+    perit1=(cd+ad+ac);
+    perit2=(db+ba+ad);
+    semit1=((perit1)/2);
+    semit2=((perit2)/2);
+
+    cout<<"Semiperimetro triangulo 1: "<<semit1<<endl;
+    cout<<"Semiperimetro triangulo 2: "<<semit2<<endl;
+    cout<<"Perimetro triangulo 1: "<<perit1<<endl;
+    cout<<"Perimetro triangulo 2: "<<perit2<<endl;
+    
+    double altura1,altura2,altura3;//alturas de triangulo 1
+    double altura4,altura5,altura6;//alturas del triangulo 2
+    
+    //altura del triangulo1
+    altura1=(2/ac*(sqrt(semit1*(semit1-ac)*(semit1-cd)*(semit1-ad))));
+    altura2=(2/cd*(sqrt(semit1*(semit1-ac)*(semit1-cd)*(semit1-ad))));
+    altura3=(2/ad*(sqrt(semit1*(semit1-ac)*(semit1-cd)*(semit1-ad))));
+    //altura del triangulo 2
+    altura4=(2/db*(sqrt(semit2*(semit2-db)*(semit2-ba)*(semit2-ad))));
+    altura5=(2/ba*(sqrt(semit2*(semit2-db)*(semit2-ba)*(semit2-ad))));
+    altura6=(2/ad*(sqrt(semit2*(semit2-db)*(semit2-ba)*(semit2-ad))));
+
+    cout<<"Las alturas del triangulo 1 miden: "<<endl;
+    cout<<"Altura 1: "<<altura1<<endl;
+    cout<<"Altura 2: "<<altura2<<endl;
+    cout<<"Altura 3: "<<altura3<<endl;
+
+    cout<<"Las alturas del triangulo 2 miden: "<<endl;
+    cout<<"Altura 1: "<<altura4<<endl;
+    cout<<"Altura 2: "<<altura5<<endl;
+    cout<<"Altura 3: "<<altura6<<endl;
+    double area;
+    area=((ad*altura1)/2)+((ad*altura4)/2);
+    cout<<"El area del trapezio es de : "<<area<<endl;
+    break;
    }
    case 3:{
     salir=true;
@@ -68,3 +152,8 @@ float e1(int x){
  }
  return total;
 }//fin ejercicio1
+
+double distancia(int x1,int y1, int x2, int y2){
+ return sqrt(pow((x2-x1),2)+pow((y2-y1),2));
+}//fin de distancia
+
